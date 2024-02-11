e690ab6e4eb8cad6ec267da0096fdc58ce93150d
ceogenbit <ceogenbit@gmail.com>
2024-02-11 18:59:36 +0100

    add preloader

--- js/app.js
+++ js/app.js
@@ -1,19 +1,6 @@
 (() => {
     "use strict";
     const flsModules = {};
-    function isWebp() {
-        function testWebP(callback) {
-            let webP = new Image;
-            webP.onload = webP.onerror = function() {
-                callback(webP.height == 2);
-            };
-            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-        }
-        testWebP((function(support) {
-            let className = support === true ? "webp" : "no-webp";
-            document.documentElement.classList.add(className);
-        }));
-    }
     function addLoadedClass() {
         if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
             setTimeout((function() {
@@ -3783,7 +3770,6 @@
         }
     }
     window["FLS"] = true;
-    isWebp();
     addLoadedClass();
     menuInit();
     formSubmit();
